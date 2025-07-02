3e741b15a4bf35a30a0395556f8fc6d5790ff6ec
unknown <shahrukhprojects@gmail.com>
2025-07-02 19:52:25 -0400

    files added

---
+++ api/WebApp/API/Program.cs
@@ -0,0 +1,24 @@
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer("name=DefaultConnection");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
